b9a7ee11d6b355aa6fc079aa7ab13864a4669ab5
SimplyOrtiz <guilherme.testando.coisas@gmail.com>
2025-10-06 22:38:42 -0300

    Delete Frontend directory
    Quickly Removing to add new files

--- Frontend/src/js/connect.js
+++
@@ -1,9 +0,0 @@
-const { json } = require("stream/consumers");
-
-async function recieveJSON() {
-    const response = await fetch('/', { method: 'POST'});
-    const content = await response.json();
-    document.getElementById('Teste').innerText = str(json);
-}
-
-setInterval(recieveJSON, 5000)
